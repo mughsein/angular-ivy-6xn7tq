41b2bb5c4236b52efee1c99d6728b2e18ddd109d
mughsein <mughishussein@gmail.com>
2021-02-02 17:21:22 +0800

    2 files modified

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,17 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
 
 export interface RouteInfo {
-    path: string;
-    title: string;
-    icon: string;
-    class: string;
+  path: string;
+  title: string;
+  icon: string;
+  class: string;
 }
 
 export const ROUTES: RouteInfo[] = [
-    { path: '/home',          title: 'Home',              icon:'nc-bank',       class: '' },
+  { path: "/home", title: "Home", icon: "nc-bank", class: "" }
+  /*
     { path: '/icons',         title: 'Icons',             icon:'nc-diamond',    class: '' },
-    /*
     { path: '/dashboard',     title: 'Dashboard',         icon:'nc-bank',       class: '' },
     { path: '/icons',         title: 'Icons',             icon:'nc-diamond',    class: '' },
     { path: '/maps',          title: 'Maps',              icon:'nc-pin-3',      class: '' },
@@ -24,14 +23,13 @@ export const ROUTES: RouteInfo[] = [
 ];
 
 @Component({
-    moduleId: module.id,
-    selector: 'sidebar-cmp',
-    templateUrl: 'sidebar.component.html',
+  moduleId: module.id,
+  selector: "sidebar-cmp",
+  templateUrl: "sidebar.component.html"
 })
-
 export class SidebarComponent implements OnInit {
-    public menuItems: any[];
-    ngOnInit() {
-        this.menuItems = ROUTES.filter(menuItem => menuItem);
-    }
+  public menuItems: any[];
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+  }
 }
